be216f6cb1441a2a821d6ec3aca79f2a2e447fde
Tristan Bennett <tmbennett2011@msn.com>
2018-01-16 08:06:06 -0700

    created style.css and index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" href="css/style.css">
+	<title>Snap CSS</title>
+	</head>
+	<body>
+		<p>What is this?</p>
+		<p>I'm no good at styling!</p>
+		<p>Maybe I'll try highlighting...</p>
+	</body>
+</html>
